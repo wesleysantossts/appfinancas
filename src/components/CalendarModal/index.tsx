0d1be48017924dda373439d977d1d78570e64033
Wesley <wesley.brazil@outlook.com>
2023-11-30 11:53:07 -0300

    tradução do calendario para ptbr

--- src/components/CalendarModal/index.tsx
+++ src/components/CalendarModal/index.tsx
@@ -7,6 +7,9 @@ import {
   ButtonFilter
 } from './styles';
 import { Calendar, LocaleConfig } from 'react-native-calendars';
+import { ptBR } from './localeCalendar';
+LocaleConfig.locales['pt-br'] = ptBR;
+LocaleConfig.defaultLocale = 'pt-br';
 
 export default function CalendarModal({setVisible, handleFilter}) {
   const [date, setDate] = useState({now: new Date(), markeddates: {}});
